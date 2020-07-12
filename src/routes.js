960f9f2d562ad306ec23f5cc4069b408657ba27e
GuilhermeFujita <fujitaguilherme@gmail.com>
2020-07-12 13:57:43 -0300

    VehicleController

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const db = require('./database/db');
-const Vehicle = db.Mongoose.model('vehicles', db.VehicleSchema, 'vehicles');
-//const Driver = db.Mongoose.model('drivers', db.DriverSchema, 'drivers');
-
-const DriverController = require('./controllers/DriverController')
+const DriverController  = require('./controllers/DriverController')
+const VehicleController = require('./controllers/VehicleController')
 
 // //Motoristas
 // router.get('/', (request, response) => {
@@ -18,54 +15,9 @@ router.put('/driver/:id', DriverController.update)
 router.delete('/driver/:id', DriverController.delete)
 
 // //Veiculos
-// //Listar veiculos
-// router.get('/vehicle', (request, response) => {
-//   Vehicle.find({}).lean().exec(function(e,vehicles){
-//     response.json(vehicles);
-//  });
-// })
-
-// // Cadastrar veiculo
-// router.post('/vehicle', (request, response) => {
-//   const newVehicle = new Vehicle({ 
-//      nomeProprietario: request.body.nomeProprietario,
-//      placa: request.body.placa,
-//      renavam: request.body.renavam
-//     });
-//     // console.log(newVehicle)
-//     newVehicle.save(function(err){
-//       if(err){
-//         response.status(500).json({ 'message': 'Erro'+ err })
-//         return
-//       }
-//       response.json(newVehicle)
-//     })
-// })
-
-// //Atualizar veiculo
-// router.put('/vehicle/:id', (request, response) => {
-//   Vehicle.findByIdAndUpdate(request.params.id, {
-//     $set:{
-//       nomeProprietario: request.body.nomeProprietario,
-//       placa: request.body.placa,
-//       renavam: request.body.renavam
-//     }
-//   }).then(() => {
-//     response.json({ 'Mensagem': 'Dados do veiculo atualizados com sucesso' })
-//   }).catch((err) => {
-//     response.json({'Erro': 'Erro ao atualizar os dados do veiculo' + err})
-//   })
-// })
-
-// //Deletar veiculo
-// router.delete('/vehicle/:id', (request, response) => {
-//   Vehicle.deleteOne({ _id: request.params.id })
-//     .then(() => {
-//       response.status(204).json({'Mensagem': 'Deletado com sucesso'})
-//     }).catch((err) => {
-//       response.status(500).json({'Erro': 'Erro ao deletar' + err})
-//     })
-// })
-
+router.get('/vehicle', VehicleController.index)
+router.post('/vehicle', VehicleController.create)
+router.put('/vehicle/:id', VehicleController.update)
+router.delete('/vehicle/:id', VehicleController.delete)
 
 module.exports = router
